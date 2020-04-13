837681db690dc7b83da98ca5f1e11b84387b2f48
Robert Smith <master_schmadoodle@outlook.com>
2020-04-13 03:18:49 +0100

    commit

--- db/db_conx.php
+++ db/db_conx.php
@@ -1,11 +1,8 @@
 <?php
     include_once("db_conf.php");
     $db_conx = mysqli_connect($db_address, $db_username, $db_pass, $db_schema);
-    // Evaluate the connection
-    if (mysqli_connect_errno()) {
-        echo mysqli_connect_error();
-        exit();
-    } else {
-        echo "Successful database connection, happy coding!!!";
-    }
+        if (mysqli_connect_errno()) {
+            echo mysqli_connect_error();
+            exit();
+        }
 ?>
