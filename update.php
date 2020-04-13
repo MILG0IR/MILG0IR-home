7156fc1f5c57a1c7131753fa13d01832a1921a8a
Robert Smith <master_schmadoodle@outlook.com>
2020-04-13 01:16:40 +0100

    commit

--- update.php
+++ update.php
@@ -1,17 +1,12 @@
-<?php
+<?
+	// CHECK IF USER IS LOGGED IN AND HAS A HIGH ENOUGH RANK
+?><?php
 	// CREATE THE VARIABLES
 		$version = array();
+		$device = string;
 	// GET CURRENT VERSION
 		$json = json_decode(file_get_contents('./etc/info.json'), TRUE);
 		$version['cur'] = preg_replace('/[^0-9]/', '', $json['Version']);
-	// RUN THE API TO CHECK FOR NEW VERSION
-		
+		$version['cur'] = $json['Device'];
 	//
-	print_r($version);
-	if($version['new'] > $version['cur']) {
-		// If there is a new update available
-	} else {
-		// If the user is running the latest version
-		echo "You are running the latest version already" . $version['cur']
-	}
 ?>
