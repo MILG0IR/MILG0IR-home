0f7dd55659dbe9fd3007eb696372854c8f573cae
Robert Smith <master_schmadoodle@outlook.com>
2020-04-13 16:53:31 +0100

    commit

--- variables.php
+++ variables.php
@@ -40,9 +40,4 @@
 		} else {
 			$ip = $_SERVER['REMOTE_ADDR'];
 		}
-	// ENCRYPTION SETTINGS
-		$mg_security = array(
-			"hash"	=>	"sha512",
-			"salt"	=>	"hVxAuijVqBRC7HtaOq4Bb5QbF2pZ9dETjqEUIt7jzhejRRuLjQJ3w5rDsRYTEjyf4TY2S9naLea5wUCHOMqpkAP9HCq9cGt7OwD3uvAuFvndoH9QibYyZL3wbfI6kplA"
-		)
 ?>
