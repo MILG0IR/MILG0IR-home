96308544b5afbea85084299406c38fe3e14db5c9
Robert Smith <master_schmadoodle@outlook.com>
2020-04-13 03:19:48 +0100

    commit

--- login.php
+++ login.php
@@ -39,9 +39,9 @@
 			}
 		</style>
 	 </head>
-	 <body bgcolor = "#FFFFFF">
-		<div align = "center">
-			<div style = "width:300px; border: solid 1px #333333; " align = "left">
+	 <body bgcolor="#FFFFFF">
+		<div align="center">
+			<div style = "width:300px; border: solid 1px #333333;" align="left">
 				<div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
 				<div style = "margin:30px">
 					<form action = "" method = "post">
