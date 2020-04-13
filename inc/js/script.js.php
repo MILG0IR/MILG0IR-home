cf4b218fdc3a7a8ae55f08985b96c01445ad66d5
Robert Smith <master_schmadoodle@outlook.com>
2020-04-13 20:44:16 +0100

    commit

--- inc/js/script.js.php
+++ inc/js/script.js.php
@@ -1,3 +1,10 @@
 <script>
-
+	$(document).ready(function() {
+		preloaderFadeOutTime = 500;
+		function hidePreloader() {
+			var preloader = $('.preloader');
+			preloader.fadeOut(preloaderFadeOutTime);
+		}
+		hidePreloader();
+	});
 </script>
