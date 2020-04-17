cbd306499fe4518310275d63e63e8b4cb8e8efd3
Robert Smith <master_schmadoodle@outlook.com>
2020-04-17 01:16:44 +0100

    minor changes

--- pages/updater.php
+++ pages/updater.php
@@ -1,8 +1,16 @@
+<?php
+
+if(isset($_GET['download'])) {
+	echo "downloading";
+} else {
+	echo "not downloading";
+}
+?>
 <script>
 	$(document).ready(function() {
 		checkforupdate(<?php echo "\"".$app_info['Version']."\", \"".$app_info['Device']."\", \"".$app_info['Branch']."\"" ?>);
 	});
 </script>
 <div class="content nav-inset">
-	
+	<h1 id="updatestatus"></h1>
 </div>
