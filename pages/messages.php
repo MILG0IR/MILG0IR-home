55baa2e698ed574d9a29df00b820d34a032a9c8e
Robert Smith <master_schmadoodle@outlook.com>
2020-04-17 12:25:02 +0100

    Minor Bug fixes

---
+++ pages/messages.php
@@ -0,0 +1,13 @@
+<?php
+	#{
+	#	"_comment": [
+	#		"WARNING: DO NOT MODIFY.",
+	#		"THE DETAILS ENTERED HERE WILL EFFECT IT'S DISPLAYED IN THE MENU."
+	#	],
+	#	"id": "2",
+	#	"title": "Messages",
+	#	"description": "",
+	#	"uri_remote": "./pages/messages.php",
+	#	"icon": ""
+	#}
+?>
