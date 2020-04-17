c5de7bb9cfa3c368786f602a848a29c833148bf6
Robert Smith <master_schmadoodle@outlook.com>
2020-04-17 16:38:13 +0100

    Add profile page

---
+++ pages/profile.php
@@ -0,0 +1,14 @@
+<?php
+	#{
+	#	"_comment": [
+	#		"WARNING: DO NOT MODIFY.",
+	#		"THE DETAILS ENTERED HERE WILL EFFECT IT'S DISPLAYED IN THE MENU."
+	#	],
+	#	"id": "998",
+	#	"title": "Profile",
+	#	"description": "",
+	#	"uri_remote": "./pages/profile.php",
+	#	"icon_category": "icons",
+	#	"icon": "user"
+	#}
+?>
