c4a6936cd8c46879f533da3d377f05b998fe4356
Robert Smith <master_schmadoodle@outlook.com>
2020-04-17 02:54:03 +0100

    minor changes

--- pages/settings.php
+++ pages/settings.php
@@ -1,1 +1,69 @@
-T.B.D
+<style>
+	body{
+		margin: 0;
+	}
+
+	.topnav {
+		overflow: hidden;
+		background-color: #333;
+	}
+
+	.topnav a{
+		float: left;
+		display: block;
+		color: #f2f2f2;
+		text-align: center;
+		padding: 14px 16px; /* top&bottom, right&left */
+		text-decoration: none;
+		font-size: 17px;
+	}
+
+	.topnav a:hover {
+		background-color: #ddd;
+		color: black;
+	}
+
+	.topnav .icon {
+		display: none;
+	}
+
+	@media only screen and (max-width: 600px) {
+		.topnav a:not(:first-child) {
+			display: none;
+		}
+		.topnav a.icon {
+			float: right;
+			display: block;
+		}
+
+		.topnav.responsive {position: relative;}
+		.topnav.responsive .icon {
+			position: absolute;
+			right: 0;
+			top: 0;
+		}
+		.topnav.responsive a {
+			float: none;
+			display: block;
+			text-align: left;
+		}
+	}
+</style>
+<div class="topnav" id="myTopnav">
+  <a href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="about">About</a>
+  <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction();">&#9776;</a>
+</div>
+
+<script>
+	function myFunction(){
+		var topnav = document.getElementById("myTopnav");
+		if (topnav.className === "topnav"){
+			topnav.className += " responsive";
+		}else{
+			topnav.className = "topnav";
+		}
+	}
+</script>
--- pages/updater.php
+++ pages/updater.php
@@ -50,8 +50,8 @@
 		}
 	}
 ?><?php
+	$log = array();
 	if(isset($_GET['download'])) {
-		$log = array();
 		$errors = array();
 		$install_stage1 = false;
 		$install_stage2 = false;
@@ -85,17 +85,6 @@
 					$install_stage2 = true;
 				}
 			}
-		// Remove the installation zip file
-			if($install_stage2) {
-				unlink($filename);
-				if(file_exists($filename)) {
-					$log[] .= "<span class='error'>Unable to remove the installation file - Minor issue, Please remove manually.</span>";
-					$errors[] .= "x";
-				} else {
-					$log[] .= "<span class='success'>The installation file has been removed successfully</span>";
-					$install_stage3 = true;
-				}
-			}
 		// COPY AND REPLACE THE EXISTING FILES
 			if($install_stage3) {
 				copy_directory($foldername, "./");
@@ -105,6 +94,30 @@
 				} else {
 					$log[] .= "<span class='success'>The required files have been installed successfully</span>";
 					$install_stage4 = true;
+				}
+			}
+		// Ammend info.json
+			// Open and decode the file
+				$json = "etc/info.json";
+				if(file_exists($json)) {
+					$jsonString = file_get_contents($json);
+					$data = json_decode($jsonString, true);
+			// Ammend the needed values
+					$data['Branch'] = $app_info['Branch'];
+					$data['Device'] = $app_info['Device'];
+			// Encode and close the file
+					$newJsonString = json_encode($data, JSON_PRETTY_PRINT);
+					file_put_contents($json, $newJsonString);
+				}
+		// Remove the installation zip file
+			if($install_stage2) {
+				unlink($filename);
+				if(file_exists($filename)) {
+					$log[] .= "<span class='error'>Unable to remove the installation file - Minor issue, Please remove manually.</span>";
+					$errors[] .= "x";
+				} else {
+					$log[] .= "<span class='success'>The installation file has been removed successfully</span>";
+					$install_stage3 = true;
 				}
 			}
 		// REMOVE THE INSTALLATION FILE
@@ -117,10 +130,7 @@
 					$log[] .= "<span class='success'>The temp installation folder has been removed successfully</span>";
 				}
 			}
-		// Echo the output
-			print_r($log);
-	} else {
-		echo "not downloading";
+		//	
 	}
 ?>
 <script>
@@ -130,5 +140,27 @@
 </script>
 <div class="content nav-inset">
 	<h1 id="updatestatus"></h1>
+	<button class="button-secondary" onclick="redirect('?download')">Force reinstall</button>
+	<div class='log'>
+		<?php
+			$arrlength = count($log);
+			if($arrlength > 0) {
+				echo "";
+					echo "<pre>";
+					echo "<h4 class='title'>LOG:</h4>";
+					echo "</br>";
+					for($x = 0; $x < $arrlength; $x++) {
+						if($x < 10) {
+							echo "0" . $x . " - " . $log[$x];
+							echo "<br>";
+						} else {
+							echo $x . " - " . $log[$x];
+							echo "<br>";
+						}
+					}
+					echo "</pre>";
+			}
+		?>
+	</div>
 	<div id="whatsnew"></div>
 </div>
