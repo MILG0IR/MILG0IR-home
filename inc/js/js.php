2b33cee9dcd9ac88ceeee01f5563b07702cd2b32
Robert Smith <master_schmadoodle@outlook.com>
2020-04-20 17:19:32 +0100

    Minor bug fixes

--- inc/js/js.php
+++ inc/js/js.php
@@ -5,7 +5,6 @@
 	include_once($mg_dir['js']."remote/twitter-bootstrap.4.0.0.alpha.6.min.js.php");
 	include_once($mg_dir['js']."remote/mdbootstrap.4.3.0.min.js.php");
 	include_once($mg_dir['js']."remote/ajax.1.0.4.min.js.php");
-	include_once($mg_dir['js']."remote/sortable.0.9.13.min.js.php");
 
 	include_once($mg_dir['js']."functions.js.php");
 	include_once($mg_dir['js']."script.js.php");
